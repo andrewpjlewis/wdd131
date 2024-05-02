b5a97522555c6020ccc867e7c90cdc9dd18856d4
LewiLewi357 <lewiand1234@gmail.com>
2024-05-02 16:31:42 -0600

    dark theme activity

---
+++ mission/scripts/mission.js
@@ -0,0 +1,14 @@
+const themeSelector = document.getElementById("dark");
+const imageElement = document.getElementById("logo");
+function changeTheme() {
+    console.log("Current theme value:", themeSelector.value);
+    if (themeSelector.value === "dark") {
+        console.log("Setting dark theme");
+        document.body.classList.add("dark");
+        imageElement.src = "images/logo_dark.png";
+    } else {
+        console.log("Setting light theme");
+        document.body.classList.remove("dark");
+    }
+}
+themeSelector.addEventListener("change", changeTheme)
