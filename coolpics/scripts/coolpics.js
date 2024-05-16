15659288b701b5778f51d47e4fce758efb5e3cd9
LewiLewi357 <lewiand1234@gmail.com>
2024-05-16 13:46:33 -0600

    cool pics part 2

--- coolpics/scripts/coolpics.js
+++ coolpics/scripts/coolpics.js
@@ -1,17 +1,41 @@
-function removePTags() {
-    var screenWidth = window.innerWidth;
-    if (screenWidth > 500) {
-        const pTags = document.getElementsByTagName('p');
-        for (var i = 0; i < pTags.length; i++) {
-            pTags[i].style.display = 'none';
-        }
-    } else {
-        const pTags = document.getElementsByTagName('p');
-        for (var i = 0; i < pTags.length; i++) {
-            pTags[i].style.display = 'block';
-        }
-    }
+const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu");
+
+function toggleMenu() {
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else if (menu.style.display === "none") {
+    menu.style.display = "flex";
+  } else {
+    menu.style.display = "flex";
+  }
 }
 
-window.onload = removePTags;
-window.onresize = removePTags;
+menuButton.addEventListener("click", toggleMenu);
+
+function handleResize() {
+  if (window.innerWidth > 700) {
+    menu.style.display = "flex";
+  } else {
+    menu.style.display = "none";
+  }
+}
+
+window.addEventListener("resize", handleResize);
+
+function viewHandler(event) {
+  const viewer = document.querySelector(".viewer");
+  if (event.target.tagName === "IMG") {
+    document.querySelector(".viewer").style.display = "grid";
+  } else {
+    document.querySelector(".viewer").style.display = "none";
+  }
+}
+
+document.querySelector(".gallery").addEventListener("click", viewHandler)
+
+function closeViewer() {
+  document.querySelector(".viewer").style.display = "none";
+}
+
+document.querySelector(".close-viewer").addEventListener("click", closeViewer)
